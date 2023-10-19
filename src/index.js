fc7053c27b00ffc6383a638f8bdff6a1f97ff195
Yu19980809 <1572250276@qq.com>
2023-10-19 15:41:38 +0800

    reset

--- src/index.js
+++ src/index.js
@@ -1,26 +1,82 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import DemoOne from './views/DemoOne'
-// import VoteFunction from './views/VoteFunction'
-// import VoteClass from './views/VoteClass'
-// import DemoTwo from './views/DemoTwo'
-// import DemoThree from './views/DemoThree'
-// import DemoFour from './views/DemoFour'
-// import DemoFive from './views/DemoFive'
-import DemoSix from './views/DemoSix'
+import './index.css'
+import { createElement, render } from './utils/jsxHandler'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(
-  <>
-    <DemoSix />
-  </>
-)
+const x = '10', y = 20
+const styleObj = {
+  color: 'pink',
+  fontSize: '24px'
+}
 
-// setTimeout(() => {
-//   root.render(
-//     <>
-//       <VoteClass title="React is bad" />
-//     </>
+// 使用自己实现的 createElement 方法创建 virtualDOM
+// console.log(
+//   createElement(
+//     'div',
+//     {className: 'container'},
+//     createElement(
+//       'h2',
+//       {className: 'title', style: styleObj},
+//       'Hello'
+//     ),
+//     createElement(
+//       'div',
+//       null,
+//       createElement('span', null, x),
+//       createElement('span', null, y)
+//     )
 //   )
-// }, 3000)
+// )
+
+// 使用内置的 React.createElement 创建 virtualDOM
+// console.log(
+//   React.createElement(
+//     'div',
+//     {className: 'container'},,
+//     React.createElement(
+//       'h2',
+//       {className: 'title', style: styleObj},
+//       'Hello'
+//     ),
+//     React.createElement(
+//       'div',
+//       null,
+//       React.createElement('span', null, x),
+//       React.createElement('span', null, y)
+//     )
+//   )
+// )
+
+// 使用内置的 render 方法渲染DOM
+// root.render(
+//   <React.StrictMode>
+//     <div className="container">
+//       <h2 className="title" style={styleObj}>Hello</h2>
+//       <div>
+//         <span>{x}</span>
+//         <span>{y}</span>
+//       </div>
+//     </div>
+//   </React.StrictMode>
+// )
+
+// 使用自己实现的 render 方法渲染DOM
+let jsxObj = createElement(
+  'div',
+  {className: 'container'},
+  createElement(
+    'h2',
+    {className: 'title', style: styleObj},
+    'Hello'
+  ),
+  createElement(
+    'div',
+    null,
+    createElement('span', null, x),
+    createElement('span', null, y)
+  )
+)
+console.log(jsxObj)
+render(jsxObj, document.getElementById('root'))
